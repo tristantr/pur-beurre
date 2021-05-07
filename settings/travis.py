804a7a10a9ae5f4812ba0421e28dface2f9b15e0
Tristan Lecuyer <tristan.use@gmail.com>
2021-05-07 14:57:07 +0200

    Add Travis files

---
+++ settings/travis.py
@@ -0,0 +1,12 @@
+from . import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '',
+        'USER': 'postgre',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
