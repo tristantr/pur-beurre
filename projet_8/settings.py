b7019d43ecb2fd9b5893b02236fe049a3260eda3
Tristan Lecuyer <tristan.use@gmail.com>
2021-05-18 12:30:09 +0200

    Add emmail reset

--- projet_8/settings.py
+++ projet_8/settings.py
@@ -112,3 +112,10 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = env('HOST_EMAIL')
+EMAIL_HOST_PASSWORD = env('HOST_PASSWORD')
